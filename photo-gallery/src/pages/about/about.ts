1b649e43b661714a0d166f717024361ecc51ace6
Marshall Epie <me@marshallepietechnologies.com>
2018-10-21 10:30:13 +0100

    apparent merge of head to branch.. different directories, 6 files modified modified:   photo-gallery/config.xml modified:   photo-gallery/package-lock.json modified:   photo-gallery/package.json modified:   photo-gallery/src/app/app.module.ts modified:   photo-gallery/src/pages/about/about.html modified:   photo-gallery/src/pages/about/about.ts

--- photo-gallery/src/pages/about/about.ts
+++ photo-gallery/src/pages/about/about.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
+
 export class AboutPage {
+ currentImage: any;
+ constructor(public navCtrl: NavController, private camera: Camera) {
+}
 
-  constructor(public navCtrl: NavController) {
-
-  }
+takePicture() {
+   const options: CameraOptions = {
+     quality: 100,
+     destinationType: this.camera.DestinationType.DATA_URL,
+     encodingType: this.camera.EncodingType.JPEG,
+     mediaType: this.camera.MediaType.PICTURE
+   }
+ 
+   this.camera.getPicture(options).then((imageData) => {
+     this.currentImage = 'data:image/jpeg;base64,' + imageData;
+   }, (err) => {
+    // Handle error
+    console.log("Camera issue:" + err);
+   });
+ }
 
 }
